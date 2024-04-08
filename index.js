1e22e4411d0ea0da904a4681502328a820c08067
Mandy <156150059+woodama2@users.noreply.github.com>
2024-04-08 19:31:24 -0400

    Exercise 2.10 - Connecting the database to the API.

--- index.js
+++ index.js
@@ -51,7 +51,12 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', {
+// mongoose.connect('mongodb://localhost:27017/cfDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
