2b4ab223e12d3ab27caecb56eb2b0258bc0062ed
Mandy <156150059+woodama2@users.noreply.github.com>
2024-07-08 11:31:43 -0400

    Changing out CORS from all origins to specific ones

--- index.js
+++ index.js
@@ -21,26 +21,26 @@ const cors = require('cors');
 const { check, validationResult } = require('express-validator');
 
 // To only allow certain origins to have access...
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-2024.netlify.app'];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn't found on the list of allowed origins
-//         let message =
-//           "The CORS policy for this application doesn't allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn't found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // To allow all origins...
-app.use(cors({}));
+// app.use(cors({}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
